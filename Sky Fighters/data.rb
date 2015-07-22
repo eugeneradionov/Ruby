4e3b9ebbf5368636685d8e7734b063f6162d5d55
Eugene Radionov <eugene.radionov@gmail.com>
2015-07-22 14:46:23 +0300

    Output bugs has been fixed

--- Sky Fighters/data.rb
+++ Sky Fighters/data.rb
@@ -2,7 +2,6 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-
 class Planes
   def initialize(name, type, nation, epoch, url)
     @name = name
@@ -17,50 +16,57 @@ end
 def best_nation(array_of_nations)
 #Determines the best nation
   max_count = -1
-  array_of_nations.each do |i|
+  for i in array_of_nations
     if max_count < i[1].to_i
       max_count = i[1].to_i
       country = i[0]
     end
   end
-
-  if country == nil
-    return 'Другие'
-  else
-    return country
+  if not country or country == 'Частные'
+    'Другие'
+    else
+      country
   end
 end
 
 def csv_out(file_name, array)
+  start = Time.now
   f = File.open(file_name, 'w')
-  f.write("Name,Type,Nation,Epoch;\n")
-  array.each{|x|
+  f.write("Name,Type,Nation,Epoch\n")
+  array.each do |x|
     if x.name.include?('/')
       x.name.gsub!('/', ' ')
     elsif x.name.include? ('"')
-      x.name.gsub!('"', '')
+      x.name.gsub!('"', '""')
     end
     f.write("#{x.name},#{x.type},#{x.nation},#{x.epoch};\n")
-  }
+  end
   f.close
+  stop = Time.now
+  time = stop - start
+  p "Export to CSV: committed #{array.size} records in #{time.to_i} seconds"
 end
 
 def json_out(file_name, array)
+  start = Time.now
   j = File.open(file_name, 'w')
   array.each{|x|
     j.write({ 'name'=> x.name, 'type' => x.type, 'nation' => x.nation, 'epoch' => x.epoch}.to_json)
   }
   j.close
+  stop = Time.now
+  time = stop - start
+  p "Export to JSON: committed #{array.size} records in #{time.to_i} seconds"
 end
 
-start = Time.now
-
-base_url = 'http://wp.scn.ru/'
+base_url = 'http://wp.scn.ru'
 wars = %w(ww1/ ww15/ ww2/ ww3/ ww4/)
 planes_types = %w(f b a t o h s v)
 planes = []
-wars.each do |war|
+count_of_pages = 0
 
+start_download = Time.now
+wars.each do |war|
   case war
     when "ww1/"
       epoch = "World War I"
@@ -87,26 +93,29 @@ wars.each do |war|
                      "v" => "Helicopter",
                      "d" => "Drone"}
     # Getting source code for planes
-    uri_planes = URI.parse("#{base_url}ru/#{war}#{type}")
+    uri_planes = URI.parse("#{base_url}/ru/#{war}#{type}")
     response_planes = Net::HTTP.get(uri_planes)
     planes_regex = /<a\shref=(?<url>[^>]*)>(?<name>[^<]*)<\/a>\s?\[\d+\]<br>/
     all_planes = response_planes.force_encoding('windows-1251').encode('UTF-8').scan(planes_regex)
-
+    count_of_pages += 1
     #Array type of [<name>, <type>, <nation>, <epoch>, <URL>]
     all_planes.each do |i|
       uri_nations = URI.parse(base_url + i[0])
       response_nations = Net::HTTP.get(uri_nations)
-      nations_regex =/<img\sclass=img_bg[^.]*\.gif>\s<a\shref=[^>]*>(?<country>[^<]*)<\/a>\s?\[(?<count>\d+)\]/
+      nations_regex = /<img\sclass=img_bg[^.]*\.gif>\s<a\shref=[^>]*>(?<country>[^<]*)<\/a>\s?\[(?<count>\d+)\]/
       nation = best_nation(response_nations.force_encoding('windows-1251').encode('UTF-8').scan(nations_regex))
+      p nation
       planes << Planes.new(i[1], type_of_plane[type], nation, epoch, base_url+i[0])
+      count_of_pages += 1
     end
   end
 end
 
+stop_download = Time.now
+time_download = (stop_download - start_download).to_i
+
+p "Fetched #{count_of_pages} pages in #{time_download} seconds."
+
 #Output into csv and json files
 csv_out('output.csv', planes)
 json_out('jsonout.json', planes)
-
-stop = Time.now
-time = stop - start
-p "Runtime: #{time.to_i} seconds"
