a8e43235cbe8d4e2a1933413ab6f32c3f05cdc61
Eugene Radionov <eugene.radionov@gmail.com>
2015-07-25 21:16:47 +0300

    Output methods have been improved.

--- Sky Fighters/data.rb
+++ Sky Fighters/data.rb
@@ -13,11 +13,14 @@ class Planes
   attr_reader :name, :type, :nation, :epoch
 end
 
-def best_nation(array_of_nations)
+def best_nation(url)
 #Determines the best nation
   begin
-    array_of_nations.max_by{|x| x[1].to_i}[0]
-    return array_of_nations.max_by{|x| x[1].to_i}[0]
+    uri_nations = URI.parse(url)
+    response_nations = Net::HTTP.get(uri_nations)
+    nations_regex = /<img\sclass=img_bg[^.]*\.gif>\s<a\shref=[^>]*>(?<country>[^<]*)<\/a>\s?\[(?<count>\d+)\]/
+    array_of_nations = response_nations.force_encoding('windows-1251').encode('UTF-8').scan(nations_regex)
+    return array_of_nations.max_by{|x| x.count.to_i}[0]
   rescue
     return 'Другие'
   end
@@ -25,14 +28,16 @@ end
 
 def csv_out(file_name, array)
   start = Time.now
-  f = File.open(file_name, 'w')
-  f.write("Name,Type,Nation,Epoch\n")
+  begin
+    f = File.open(file_name, 'w')
+    f.write("Name,Type,Nation,Epoch\n")
+  rescue IOError
+    return p 'I/O error!'
+  rescue
+    return p 'Oops :('
+  end
   array.each do |x|
-    if x.name.include?('/')
-      x.name.gsub!('/', ' ')
-    elsif x.name.include? ('"')
-      x.name.gsub!('"', '""')
-    end
+    x.name.gsub!('"', '""')
     f.write("\"#{x.name}\",\"#{x.type}\",\"#{x.nation}\",\"#{x.epoch}\"\n")
   end
   f.close
@@ -42,7 +47,13 @@ end
 
 def json_out(file_name, array)
   start = Time.now
-  j = File.open(file_name, 'w')
+  begin
+    j = File.open(file_name, 'w')
+  rescue IOError
+    return p 'I/O error!'
+  rescue
+    return p 'Oops :('
+  end
   array.each{|x|
     j.write({ 'name'=> x.name, 'type' => x.type, 'nation' => x.nation, 'epoch' => x.epoch}.to_json)
   }
@@ -51,57 +62,6 @@ def json_out(file_name, array)
   p "Export to JSON: committed #{array.size} records in #{time} seconds"
 end
 
-=begin
-planes = []
-base_url = 'http://wp.scn.ru'
-wars = %w(ww1/ ww15/ ww2/ ww3/ ww4/)
-planes_types = %w(f b a t o h s v)
-
-count_of_pages = 0
-wars.each do |war|
-  case war
-    when "ww1/"
-      epoch = "World War I"
-    when "ww15/"
-      epoch = "Interwar"
-    when "ww2/"
-      epoch = "World War II"
-    when "ww3/"
-      epoch = "Cold war"
-    when "ww4/"
-      epoch = "Modern"
-      planes_types = %w(f b a t o h s v d)
-    else
-      epoch = "Unknown"
-  end
-  planes_types.each do |type|
-    type_of_plane = {"f" => "Fighter",
-                     "b" => "Bomber",
-                     "a" => "Attack",
-                     "t" => "Transport",
-                     "o" => "Other",
-                     "h" => "Sea",
-                     "s" => "Special",
-                     "v" => "Helicopter",
-                     "d" => "Drone"}
-    #Array type of [<URL>, <name>]
-    uri_planes = URI.parse("#{base_url}/ru/#{war}#{type}")
-    response_planes = Net::HTTP.get(uri_planes)
-    planes_regex = /<a\shref=(?<url>[^>]*)>(?<name>[^<]*)<\/a>\s?\[\d+\]<br>/
-    all_planes = response_planes.force_encoding('windows-1251').encode('UTF-8').scan(planes_regex)
-    count_of_pages += 1
-    #Array type of [<name>, <type>, <nation>, <epoch>, <URL>]
-    all_planes.each do |i|
-      uri_nations = URI.parse(base_url + i[0])
-      response_nations = Net::HTTP.get(uri_nations)
-      nations_regex = /<img\sclass=img_bg[^.]*\.gif>\s<a\shref=[^>]*>(?<country>[^<]*)<\/a>\s?\[(?<count>\d+)\]/
-      nation = best_nation(response_nations.force_encoding('windows-1251').encode('UTF-8').scan(nations_regex))
-      planes << Planes.new(i[1], type_of_plane[type], nation, epoch, base_url+i[0])
-      count_of_pages += 1
-    end
-  end
-end
-=end
 start_download = Time.now
 planes = []
 count_of_pages = 0
@@ -154,12 +114,10 @@ urls.each do |plane|
   response_planes = Net::HTTP.get(uri_planes)
   planes_regex = /<a\shref=(?<url>[^>]*)>(?<name>[^<]*)<\/a>\s?\[\d+\]<br>/
   all_planes = response_planes.force_encoding('windows-1251').encode('UTF-8').scan(planes_regex)
+
+  #Array type of [<name>, <type>, <nation>, <epoch>, <URL>]
   all_planes.each do |i|
-    #Array type of [<name>, <type>, <nation>, <epoch>, <URL>]
-    uri_nations = URI.parse(plane[0] + i[0].split('/')[-1])
-    response_nations = Net::HTTP.get(uri_nations)
-    nations_regex = /<img\sclass=img_bg[^.]*\.gif>\s<a\shref=[^>]*>(?<country>[^<]*)<\/a>\s?\[(?<count>\d+)\]/
-    nation = best_nation(response_nations.force_encoding('windows-1251').encode('UTF-8').scan(nations_regex))
+    nation = best_nation(plane[0] + i[0].split('/')[-1])
     planes << Planes.new(i[1], plane[1], nation, plane[2], plane[0]+i[0])
     count_of_pages += 1
   end
@@ -167,6 +125,7 @@ end
 
 time_download = (Time.now - start_download).to_i
 p "Fetched #{count_of_pages} pages in #{time_download} seconds."
+
 #Output into csv and json files
 csv_out('output.csv', planes)
 json_out('jsonout.json', planes)
